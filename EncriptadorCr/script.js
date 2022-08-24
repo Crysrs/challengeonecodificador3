d0b3c6070a5993c9008f287f9c8dbdf1a8141818
Crystian Romero Sanchez <112023740+Crysrs@users.noreply.github.com>
2022-08-24 15:58:41 -0500

    Add files via upload

---
+++ EncriptadorCr/script.js
@@ -0,0 +1,49 @@
+
+let inputTexto = document.querySelector(".input-text")
+let mensaje = document.querySelector(".mensaje")
+
+function btnEncriptar() {
+    let textoEncriptado = encriptar(inputTexto.value);
+    mensaje.value = textoEncriptado;
+    mensaje.style.backgroundImage = "none";
+    inputTexto.value = "";
+}
+
+function encriptar(stringEncriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for( let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1])
+        }
+    }
+    return stringEncriptada;
+
+}
+
+function btnDesencriptar() {
+    let textoEncriptado = desencriptar(inputTexto.value);
+    mensaje.value = textoEncriptado;
+    inputTexto.value = "";
+}
+
+function desencriptar(stringDesencriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
+    stringDencriptada = stringDesencriptada.toLowerCase();
+
+    for( let i = 0; i < matrizCodigo.length; i++){
+        if(stringDesencriptada.includes(matrizCodigo[i][1])){
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1],matrizCodigo[i][0])
+        }
+    }
+    return stringDesencriptada;
+
+}
+
+function copiar(){
+    mensaje.select()
+    navigator.clipboard.writeText(mensaje.value)
+    mensaje.value = "";
+
+}
